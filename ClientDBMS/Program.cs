8db8be6c5cacfe28efd9009fa51ed8e4043fde81
Aderibigbe Adeola <115571361+Adeola-Aderibigbe@users.noreply.github.com>
2022-10-18 17:05:37 +0100

    Added DI

--- ClientDBMS/Program.cs
+++ ClientDBMS/Program.cs
@@ -10,15 +10,12 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<SessionFactory>();
+builder.Services.AddSingleton<AppConfiguration>();
 builder.Services.AddScoped<IClient, ClientRepository>();
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
     app.UseSwagger();
     app.UseSwaggerUI();
-//}
 
 app.UseHttpsRedirection();
 
