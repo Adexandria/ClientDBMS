14577c103183275432e88a68e82b9df41fc3044e
Aderibigbe Adeola <115571361+Adeola-Aderibigbe@users.noreply.github.com>
2022-10-18 17:05:08 +0100

    created a custom app configuration class

---
+++ ClientDBMS/Services/AppConfiguration.cs
@@ -0,0 +1,17 @@
+﻿namespace ClientDBMS.Services
+{
+    public class AppConfiguration 
+    {
+        private IConfiguration _configuration;
+
+        public AppConfiguration(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GetConnectionString()
+        {
+            return _configuration.GetConnectionString("DefaultConnection");
+        }
+    }
+}
