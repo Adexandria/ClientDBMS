10509e88d09454bcc1c7e6491a79055ccee50dd6
Aderibigbe Adeola <115571361+Adeola-Aderibigbe@users.noreply.github.com>
2022-10-19 10:29:56 +0100

    fixed

--- ClientDBMS/Services/AppConfiguration.cs
+++ ClientDBMS/Services/AppConfiguration.cs
@@ -11,7 +11,7 @@
 
         public string GetConnectionString()
         {
-            return _configuration.GetConnectionString("DefaultConnections");
+            return _configuration.GetConnectionString("DefaultConnection");
         }
     }
 }
