5c9f6f9257a7befe7eed0d3ba89264146a5a8289
Aderibigbe Adeola <115571361+Adeola-Aderibigbe@users.noreply.github.com>
2022-10-19 11:55:44 +0100

    testing for app setting

---
+++ ClientDBMS/Controllers/TestController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClientDBMS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        private IConfiguration _config;
+        public TestController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpGet("connection-string")]
+        public IActionResult TestConnectionString()
+        {
+            var connectionString = _config.GetConnectionString("DefaultConnection");
+            return Ok(connectionString);
+        }
+
+        [HttpGet("message")]
+        public IActionResult GetMessage()
+        {
+            var messages = _config.GetSection("Message");
+            var chatMessage = messages["Chat"];
+            return Ok(chatMessage);
+        }
+    }
+}
